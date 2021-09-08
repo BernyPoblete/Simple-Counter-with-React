67b7f6c23d784fb5f37d014b86931368bf58b6a7
BernyPoblete <btapoblete@gmail.com>
2021-09-08 03:56:06 +0000

    falta icono

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,8 +4,13 @@ import ReactDOM from "react-dom";
 
 //My first component
 function SecondsCounter(props) {
+	//El operador resto (%) devuelve el resto de la división entre dos operandos. Siempre toma el signo del dividendo.
+	//en first: digitOne=11 entonces 11 % 10= 1, 12 % 10= 2, 13 % 10= 3... siempre entrega 1 digito (unidad del counter)
 	return (
 		<div className="bigDiv">
+			<div className="icon">
+				<i className="far fa-clock"></i>
+			</div>
 			<div className="sixth">{props.digitSix % 10}</div>
 			<div className="fiveth">{props.digitFive % 10}</div>
 			<div className="fourth">{props.digitFour % 10}</div>
@@ -32,12 +37,18 @@ setInterval(() => {
 	//setInterval() hace que una función se repita con un tiempo de retraso entre cada ejecución.
 	//Se pasan dos parámetros a setInterval(): la función que deseo llamar y el tiempo en milisegundos a retrasar cada ejecución de la función.
 	//setInterval() continuará ejecutándose hasta que sea borrada. clearInterval().
-	const six = Math.floor(counter / 100000); //Math.floor(x)   Devuelve el máximo entero menor o igual a un número
+	const six = Math.floor(counter / 100000);
+	//Math.floor(x)   Devuelve el máximo entero menor o igual a un número
 	const five = Math.floor(counter / 10000);
 	const four = Math.floor(counter / 1000);
 	const three = Math.floor(counter / 100);
 	const two = Math.floor(counter / 10);
+	// counter = 11 entonces 11/10 = 1,1 redondeado a 1
 	const one = Math.floor(counter / 1);
+	// counter =11  11/1= 11
+	counter++;
+	console.log("pasando por aquí");
+	//probando funcion en consola
 	ReactDOM.render(
 		<SecondsCounter
 			digitOne={one}
@@ -49,6 +60,7 @@ setInterval(() => {
 		/>,
 		document.querySelector("#app")
 	);
-}, 1000); //segundo parametro de setInterval, cada 1 segundo
+}, 1000);
+//segundo parametro de setInterval, cada 1 segundo
 
 export default SecondsCounter;
